e32ca2ac88e4605214864d9c0e805ae83770180b
Nick Montimurro <nmonti917@gmail.com>
2026-06-02 17:22:42 -0400

    Delete capacitor.config.ts

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.monti.jquest',
-  appName: 'JQuest Japanese Tutor',
-  webDir: 'dist'
-};
-
-export default config;
